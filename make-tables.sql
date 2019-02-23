46d93da3deeeeb58a2ddc5373c2717c66da64858
cmpeters08 <cmpeters08@gmail.com>
2019-02-23 13:47:41 -0600

    added create table query

---
+++ make-tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE USERS (user_id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, role VARCHAR(255), is_active BOOLEAN NOT NULL, last_updated TIMESTAMP NOT NULL);
+
+CREATE TABLE PROVIDERS (provider_id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, hours VARCHAR(255) NOT NULL, days_of_operation VARCHAR(7), last_verified DATE, is_active BOOLEAN, last_updated TIMESTAMP NOT NULL);
+
+CREATE TABLE REFERALS (referal_id SERIAL PRIMARY KEY, referer_id INTEGER REFERENCES PROVIDERS(provider_id) NOT NULL, referer_name VARCHAR(255), referee_id INTEGER REFERENCES PROVIDERS(provider_id) NOT NULL, referee_name VARCHAR(255), is_eligiable BOOLEAN, date_of_refreal DATE, fufillment_stats VARCHAR(255), last_updated TIMESTAMP NOT NULL);
+
+CREATE TABLE CRIBS (provider_id INTEGER REFERENCES PROVIDERS(provider_id) NOT NULL, provider_name VARCHAR(255), crib_count INTEGER NOT NULL, crib_type VARCHAR(255), last_updated TIMESTAMP NOT NULL);
+
+CREATE TABLE ADDRESSES (address_id SERIAL PRIMARY KEY, provider_id INTEGER REFERENCES PROVIDERS(provider_id) NOT NULL, primary_site boolean, class_id INTEGER, name VARCHAR(255), street VARCHAR(255) NOT NULL, line_2 VARCHAR(255), city VARCHAR(255), state char(2), zip_code CHAR(5), is_active BOOLEAN NOT NULL, last_updated TIMESTAMP NOT NULL);
+
+CREATE TABLE WAITLIST (waitlist_id SERIAL PRIMARY KEY, provider_id INTEGER REFERENCES PROVIDERS(provider_id) NOT NULL, date_of_waitlist DATE, is_eligiable BOOLEAN, is_active BOOLEAN, last_updated TIMESTAMP NOT NULL);
+
+CREATE TABLE CLASSES (provider_id INTEGER REFERENCES PROVIDERS(provider_id) NOT NULL, class_name VARCHAR(255), class_description text, address_id INTEGER REFERENCES ADDRESSES(address_id) NOT NULL, date_of_class DATE, time_of_class time, current_attendee_count INTEGER, max_attendees INTEGER, class_status VARCHAR(255), is_active BOOLEAN, last_updated TIMESTAMP NOT NULL);
+
+-- CREATE TABLE ELIGIBILGY_REQUIREMENTS (provider_id INTEGER REFERENCES PROVIDERS(provider_id), gov_assistance BOOLEAN NOT NULL, mother_under_17 boolean NOT NULL, );
