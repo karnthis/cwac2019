c387178df2b08cc20d9f23ae36b2bc1be7c02e33
Erin <erin@atomicbear.com>
2019-02-24 14:22:15 -0600

    final push on Sunday

--- local/api-nodejs/index.js
+++ local/api-nodejs/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const { Pool } = require('pg')
-const Passport = require('passport')
+// const Passport = require('passport')
 
 let count = 0
 let isProd = false
@@ -22,14 +22,7 @@ const PGPool = new Pool({
 	port: PGPORT,
 })
 
-/**
- * get logger
- * 1: console + file
- * 2: file
- * 3: error
- */
-
-// todo import models
+// todo import models?
 
 // set up express
 const app = express();
@@ -70,35 +63,65 @@ app.use(cors(corsConfig));
 // GET paths
 app.get('/', (req, res) => {
 	count += 1
-	PGPool.query('SELECT NOW()', (err, queryRes) => {
-		console.log(err, queryRes)
-	})
+	// PGPool.query('SELECT NOW()', (err, queryRes) => {
+	// 	console.log(err, queryRes)
+	// })
 	res.send(`I have been visited ${count} times!`)
 })
 
 app.get('/elegibility/:org?', (req, res) => {
-	let ret = req.params.org | '~none~'
-	// count += 1
-	res.send(`I am with: ${req.params.org}!`)
+	const org = req.params.org || ''
+	let sql = `SELECT * FROM ELIGIBILITY_REQUIREMENTS `
+	if (org) {
+		const allOrgs = org.split('-')
+		sql += `WHERE provider_id in (${arrayToString(allOrgs)})`
+	}
+	runQuery(sql, res)
 })
 
 app.get('/inventory/:org?/', (req, res) => {
-	count += 1
-	res.send(`I have ${count} many!`)
+	const org = req.params.org || ''
+	let sql = `SELECT * FROM CRIBS `
+	if (org) {
+		const allOrgs = org.split('-')
+		sql += `WHERE provider_id in (${arrayToString(allOrgs)})`
+	}
+	runQuery(sql, res)
 })
 
 app.get('/calendar/:org?/', (req, res) => {
-	count += 1
-	res.send(`I have ${count} events!`)
+	const org = req.params.org || ''
+	let sql = `SELECT * FROM CLASSES WHERE is_active = true `
+	if (org) {
+		const allOrgs = org.split('-')
+		sql += `AND provider_id in (${arrayToString(allOrgs)})`
+	}
+	runQuery(sql, res)
 })
 
-app.get('/tables', (req, res) => {
-	PGPool.query(`SELECT * FROM pg_catalog.pg_tables WHERE tablename NOT LIKE 'pg_%' AND tablename NOT LIKE 'sql_%'`, (err, queryRes) => {
-		if (err) console.log(err)
-		res.send(queryRes.rows)
-	})
-	
+app.get('/provider/:org?/', (req, res) => {
+	//todo	filter return
+	const org = req.params.org || ''
+	let sql = `SELECT * FROM PROVIDERS WHERE is_active = true `
+	if (org) {
+		const allOrgs = org.split('-')
+		sql += `AND provider_id in (${arrayToString(allOrgs)})`
+	}
+	runQuery(sql, res)
 })
+
+//todo
+app.get('/county/:zip?/', (req, res) => {
+	//todo	filter return
+	const reqFilter = req.params.zip || ''
+	let sql = `SELECT * FROM COUNTIES `
+	if (reqFilter) {
+		const allFilters = reqFilter.split('--')
+		sql += `WHERE county_id in (${arrayToString(allFilters)})`
+	}
+	runQuery(sql, res)
+})
+
 
 // app.get('/', (req, res) => {
 // 	count += 1
@@ -164,25 +187,6 @@ app.post('/auth', (req, res) => {
 	res.send(responseObject)
 })
 
-const mustMatch = 'updateMe'
-app.post('/runsql', (req, res) => {
-	// console.log(req.body)
-	const { sql, code } = req.body
-	const queryObject = {
-		text: sql
-	}
-	if (code === mustMatch) {
-		PGPool.query(queryObject, (err, queryRes) => {
-			console.log(err, queryRes)
-			res.statusCode = 200
-			res.send('Insert Successful')
-		})
-	} else {
-		res.statusCode = 401
-		res.send('Not Authorized')
-	}
-})
-
 //todo
 app.post('/adduser', (req, res) => {
 	// console.log(req.body)
@@ -244,53 +248,69 @@ app.post('/addprovider', (req, res) => {
 })
 
 //todo
-app.post('/addinventory', (req, res) => {
-	// console.log(req.body)
-	const { uname, email, pass } = req.body
-	const queryObject = {
-		text: `INSERT INTO PROVIDERS ('name', 'phone', 'description', 'hours', 'days_of_operation', 'last_verified', 'is_active', 'last_updated') VALUES ()`,
-		values: ['uname', 'email', 'pass'],
-	}
-	if (code === mustMatch) {
-		PGPool.query(queryObject, (err, queryRes) => {
-			console.log(err, queryRes)
-			res.statusCode = 200
-			res.send('Insert Successful')
-		})
-	} else {
-		res.statusCode = 401
-		res.send('Not Authorized')
-	}
-})
-
-//todo
-app.post('/addreferral', (req, res) => {
-	// console.log(req.body)
-	const { uname, email, pass } = req.body
-	const queryObject = {
-		text: `INSERT INTO PROVIDERS ('name', 'phone', 'description', 'hours', 'days_of_operation', 'last_verified', 'is_active', 'last_updated') VALUES ()`,
-		values: ['uname', 'email', 'pass'],
-	}
-	if (code === mustMatch) {
-		PGPool.query(queryObject, (err, queryRes) => {
-			console.log(err, queryRes)
-			res.statusCode = 200
-			res.send('Insert Successful')
-		})
-	} else {
-		res.statusCode = 401
-		res.send('Not Authorized')
-	}
-})
-
-//todo
 app.post('/add/:addType', (req, res) => {
 	// console.log(req.body)
-	let tmp = req.params.addType
-	const { uname, email, pass } = req.body
+	const { addType } = req.params
+	req.body.is_active = true
+	// req.body
+	// switch (addType) {
+	// 	case 'USERS':
+
+	// 		break;
+	// 	case 'PROVIDERS':
+
+	// 		break;
+	// 	case 'REFERRALS':
+
+	// 		break;
+	// 	case 'CRIBS':
+
+	// 		break;
+	// 	case 'ADDRESSES':
+
+	// 		break;
+	// 	case 'WAITLIST':
+
+	// 		break;
+	// 	case 'CLASSES':
+
+	// 		break;
+	// 	case 'ELIGIBILITY_REQUIREMENTS':
+
+	// 		break;
+	// }
+
+	const beStamped = makeTimestamp()
 	const queryObject = {
-		text: `INSERT INTO PROVIDERS ('name', 'phone', 'description', 'hours', 'days_of_operation', 'last_verified', 'is_active', 'last_updated') VALUES ()`,
-		values: ['uname', 'email', 'pass'],
+		values: []
+	}
+	let colArr4Str = []
+	let valueArr4Str = []
+	let count = 0 
+	for (key in req.body) {
+		count += 1
+		valueArr4Str.push(`\$${count}`)
+		colArr4Str.push(`${key}`)
+		queryObject.values.push(req.body[key])
+	}
+	const colStringPart = colArr4Str.join(`', '`)
+	const valStringPart = valueArr4Str.join(`, `)
+	queryObject.text = `INSERT INTO ${addType} ('${colStringPart}') VALUES (${valStringPart})`;
+
+	PGPool.query(queryObject, (err, queryRes) => {
+		console.log(err, queryRes)
+		res.statusCode = 200
+		res.send(`Insert Successful: ${queryRes}`)
+	})
+})
+
+// Dev Routes
+const mustMatch = 'updateMe'
+app.post('/runsql', (req, res) => {
+	// console.log(req.body)
+	const { sql, code } = req.body
+	const queryObject = {
+		text: sql
 	}
 	if (code === mustMatch) {
 		PGPool.query(queryObject, (err, queryRes) => {
@@ -302,14 +322,65 @@ app.post('/add/:addType', (req, res) => {
 		res.statusCode = 401
 		res.send('Not Authorized')
 	}
+})
+
+app.get('/tables', (req, res) => {
+	PGPool.query(`SELECT * FROM pg_catalog.pg_tables WHERE tablename NOT LIKE 'pg_%' AND tablename NOT LIKE 'sql_%'`, (err, queryRes) => {
+		if (err) console.log(err)
+		handleSuccess({data: queryRes.rows})
+	})
 })
 
 // Functions
-
 function makeTimestamp() {
 	return Date.now()
 }
 
+function runQuery(sql, res) {
+	PGPool.query(sql, (err, queryRes) => {
+		if (err) {
+			handleError(res, {
+				err: err,
+				msg: 'Query Error',
+				code: 400,
+			})
+		} else {
+			handleSuccess(res, {
+				data: queryRes.rows,
+			})
+		}
+	})
+}
+
+function arrayToString(array) {
+	let retString = ''
+	for (const i in array) {
+		const isInt = parseInt(array[i]) || -1
+		retString += `${isInt}, `
+	}
+	return retString.slice(0, -2) //* trim trailing ', '
+}
+
+function handleError(res, errorObj) {
+	const { code, msg, err } = errorObj
+	const body = {
+		message: msg,
+		error: err,
+	}
+	console.log(err)
+	res.status(code)
+	res.send(JSON.stringify(body))
+}
+
+function handleSuccess(res, successObj) {
+	const { code, data } = successObj
+	// const body = {
+	// 	message: msg,
+	// 	error: err,
+	// }
+	res.status(200)
+	res.send(JSON.stringify(data))
+}
 
 // Start server
 let port = process.env.PROD_PORT || 51515;
