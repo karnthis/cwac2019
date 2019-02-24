017add868ab3d02b926e7a347eeb8834680bc97a
cmpeters08 <cmpeters08@gmail.com>
2019-02-23 20:46:24 -0600

    added provider requirement criteria, added username and password to users table

--- make-tables.sql
+++ make-tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE USERS (user_id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, role VARCHAR(255), is_active BOOLEAN NOT NULL, last_updated TIMESTAMP NOT NULL);
+CREATE TABLE USERS (user_id SERIAL PRIMARY KEY, username varchar(255) NOT NULL UNIQUE, password TEXT NOT NULL,name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, role VARCHAR(255), is_active BOOLEAN NOT NULL, last_updated TIMESTAMP NOT NULL);
 
 CREATE TABLE PROVIDERS (provider_id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, description text, hours VARCHAR(255) NOT NULL, days_of_operation VARCHAR(7), last_verified DATE, is_active BOOLEAN, last_updated TIMESTAMP NOT NULL);
 
@@ -12,4 +12,5 @@ CREATE TABLE WAITLIST (waitlist_id SERIAL PRIMARY KEY, provider_id INTEGER REFER
 
 CREATE TABLE CLASSES (provider_id INTEGER REFERENCES PROVIDERS(provider_id) NOT NULL, class_name VARCHAR(255), class_description text, address_id INTEGER REFERENCES ADDRESSES(address_id) NOT NULL, date_of_class DATE, time_of_class time, current_attendee_count INTEGER, max_attendees INTEGER, class_status VARCHAR(255), is_active BOOLEAN, last_updated TIMESTAMP NOT NULL);
 
--- CREATE TABLE ELIGIBILITY_REQUIREMENTS (provider_id INTEGER REFERENCES PROVIDERS(provider_id), gov_assistance BOOLEAN NOT NULL, mother_under_17 boolean NOT NULL, );
+CREATE TABLE ELIGIBILITY_REQUIREMENTS (provider_id INTEGER REFERENCES PROVIDERS(provider_id), public_assistance BOOLEAN NOT NULL, mother_17_or_younger boolean NOT NULL, is_client BOOLEAN NOT NULL, client_id INTEGER REFERENCES ADDRESSES(address_id), months_pregnant BOOLEAN NOT NULL, residency_requirement BOOLEAN NOT NULL, must_meet_all_criteria boolean not null);
+
