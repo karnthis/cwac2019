842bf50104ca4b41fbd10910fcc1078d6cfe0516
Erin <erin@atomicbear.com>
2019-04-03 21:50:15 -0500

    more fixes to eligibility

--- api-nodejs/controllers/v1/eligibility.js
+++ api-nodejs/controllers/v1/eligibility.js
@@ -53,16 +53,24 @@ orgidGet.validate = [
 ]
 
 orgidGet.func = async (req, res) => {
-	const {
-		rows
-	} = await DB.query(`SELECT provider_id, ${iColsS} FROM ${iTbl} LEFT JOIN ${gTbl} using(elig_group_id) WHERE provider_id = ${req.params.orgid}`)
-	const ret = {}
-	rows.forEach(el => {
-		const pid = el.provider_id
-		if (!ret[pid]) ret[pid] = []
-		ret[pid].push(el)
-	})
-	res.status(200).json(ret)
+	const errors = validationResult(req)
+	if (errors.isEmpty()) {
+		const {
+			rows
+		} = await DB.query(`SELECT provider_id, ${iColsS} FROM ${iTbl} LEFT JOIN ${gTbl} using(elig_group_id) WHERE provider_id = ${req.params.orgid}`)
+		const ret = {}
+		rows.forEach(el => {
+			const pid = el.provider_id
+			if (!ret[pid]) ret[pid] = []
+			ret[pid].push(el)
+		})
+		res.status(200).json(ret)
+	} else {
+		console.log('error')
+		return res.status(422).json({
+			errors: errors.array()
+		})
+	}	
 }
 
 orgidPost.validate = [
@@ -70,12 +78,20 @@ orgidPost.validate = [
 ]
 
 orgidPost.func = async (req, res) => {
-	const {
-		rows
-	} = await DB.query(`INSERT INTO ${gTbl} (provider_id) VALUES (${req.params.orgid}) RETURNING elig_group_id`)
-	res.status(200).json({
-		rows
-	})
+	const errors = validationResult(req)
+	if (errors.isEmpty()) {
+		const {
+			rows
+		} = await DB.query(`INSERT INTO ${gTbl} (provider_id) VALUES (${req.params.orgid}) RETURNING elig_group_id`)
+		res.status(200).json({
+			rows
+		})
+	} else {
+		console.log('error')
+		return res.status(422).json({
+			errors: errors.array()
+		})
+	}
 }
 
 gidPost.validate = [
@@ -87,13 +103,21 @@ gidPost.validate = [
 ]
 
 gidPost.func = async (req, res) => {
-	const { elig_item_type_code, elig_item_type_label, elig_item_value } = req.body
-	const {
-		rows
-	} = await DB.query(`INSERT INTO ${iTbl} (${iColsI}) VALUES (${req.params.orgid}, ${elig_item_type_code}, '${elig_item_type_label}', '${elig_item_value}') RETURNING elig_item_id`)
-	res.status(200).json({
-		rows
-	})
+	const errors = validationResult(req)
+	if (errors.isEmpty()) {
+		const { elig_item_type_code, elig_item_type_label, elig_item_value } = req.body
+		const {
+			rows
+		} = await DB.query(`INSERT INTO ${iTbl} (${iColsI}) VALUES (${req.params.orgid}, ${elig_item_type_code}, '${elig_item_type_label}', '${elig_item_value}') RETURNING elig_item_id`)
+		res.status(200).json({
+			rows
+		})
+	} else {
+		console.log('error')
+		return res.status(422).json({
+			errors: errors.array()
+		})
+	}
 }
 
 gidPut.validate = [
@@ -101,25 +125,31 @@ gidPut.validate = [
 	param('gid').isInt(),
 	param('iid').isInt(),
 	check('elig_item_type_code').optional().isInt(),
-	check('elig_item_type_label').trim().escape(),
-	check('elig_item_value').trim().escape(),
+	check('elig_item_type_label').optional().trim().escape(),
+	check('elig_item_value').optional().trim().escape(),
 ]
 
 gidPut.func = async (req, res) => {
-	//todo
-	const { elig_item_type_code, elig_item_type_label, elig_item_value } = req.body
-	const toUpdate = []
-	if (elig_item_type_code) toUpdate.push(`elig_item_type_code = ${elig_item_type_code}`)
-	if (elig_item_type_label) toUpdate.push(`elig_item_type_label = ${elig_item_type_label}`)
-	if (elig_item_value) toUpdate.push(`elig_item_value = ${elig_item_value}`)
-	toUpdate.join(', ')
-
-	const {
-		rows
-	} = await DB.query(`UPDATE ${iTbl} SET ${toUpdate} WHERE elig_item_id = ${iTbl}`)
-	res.status(200).json({
-		rows
-	})
+	const errors = validationResult(req)
+	if (errors.isEmpty()) {
+		const { elig_item_type_code, elig_item_type_label, elig_item_value } = req.body
+		const toUpdate = []
+		if (elig_item_type_code) toUpdate.push(`elig_item_type_code = ${elig_item_type_code}`)
+		if (elig_item_type_label) toUpdate.push(`elig_item_type_label = ${elig_item_type_label}`)
+		if (elig_item_value) toUpdate.push(`elig_item_value = ${elig_item_value}`)
+		toUpdate.join(', ')
+		const {
+			rows
+		} = await DB.query(`UPDATE ${iTbl} SET ${toUpdate} WHERE elig_item_id = ${iTbl}`)
+		res.status(200).json({
+			rows
+		})
+	} else {
+		console.log('error')
+		return res.status(422).json({
+			errors: errors.array()
+		})
+	}
 }
 
 // EXPORT ROUTES
