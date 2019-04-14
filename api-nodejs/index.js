b17218666ba0e254ddcac6def51eec8e7fbcddda
Erin <erin@atomicbear.com>
2019-04-14 14:53:49 -0500

    adjusted default port process

--- api-nodejs/index.js
+++ api-nodejs/index.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 
 // Misc ENV vars
-const { NODE_ENV, IS_PROD, COOKIE_CODE } = process.env
+const { NODE_ENV, IS_PROD, COOKIE_CODE, PROD_PORT, IS_LOCAL } = process.env
 if (NODE_ENV == 'prod' || IS_PROD == 'yes') isProd = true
 
 // set up express
@@ -33,7 +33,7 @@ require('./routes/v1')(app)
 app.use('/dev', require('./routes/dev'))
 
 // Start server //
-let port = process.env.PROD_PORT || 5555;
+let port = (IS_LOCAL == 'yes') ? 5555 : PROD_PORT || 51515;
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
 });
